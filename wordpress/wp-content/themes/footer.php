710a8f944f8bc8d4b4d34ab50eb4e9e22e957c0d
John Doe <johndoe@example.com>
2020-04-27 21:27:24 +0200

    Extraire le header / footer

---
+++ wordpress/wp-content/themes/footer.php
@@ -0,0 +1,44 @@
+    <!-- s-footer
+    ================================================== -->
+    <footer class="s-footer">
+
+        <div class="s-footer__main">
+            <div class="row">
+
+                <div class="col-two md-four mob-full s-footer__sitelinks">
+
+                    <h4>Quick Links</h4>
+
+
+                </div> <!-- end s-footer__sitelinks -->
+
+
+                <div class="col-five md-full end s-footer__subscribe">
+
+                    <h4>Information</h4>
+
+
+
+                </div> <!-- end s-footer__subscribe -->
+
+            </div>
+        </div> <!-- end s-footer__main -->
+
+        <div class="s-footer__bottom">
+            <div class="row">
+                <div class="col-full">
+                    <div class="s-footer__copyright">
+
+                    </div>
+
+                    <div class="go-top">
+                        <a class="smoothscroll" title="Back to Top" href="#top"></a>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- end s-footer__bottom -->
+
+    </footer> <!-- end s-footer -->
+    </body>
+
+    </html>
